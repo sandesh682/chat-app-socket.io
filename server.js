d0c176c188235d4e81a2839ac9404d2318ce9cc7
Sandesh Nangare <sandesh682@gmail.com>
2021-03-11 18:13:47 +0530

    chat.html and index.html developed with better UI

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, "public")))
+
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
